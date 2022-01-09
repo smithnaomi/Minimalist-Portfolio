d6aec1e3cb69e0d0949c58b9bdd0f7b6c034d430
smithnaomi <88051105+smithnaomi@users.noreply.github.com>
2022-01-09 13:53:33 -0500

    import react, function, return and export

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navigation from "../Navigation";
+
+function Header() {
+  return (
+    <div>
+      <Navigation></Navigation>
+    </div>
+  );
+}
+export default Header;
